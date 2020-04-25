3337ec3cb71c5fd98fd39b1e1eac9d43acda05bf
Keshav Naharwar <Knaharwar@gmail.com>
2020-04-25 09:54:55 +0530

    update project and delete project completed and update in spring-boot app

--- ppmtoolapi/src/main/java/com/example/demo/web/ProjectController.java
+++ ppmtoolapi/src/main/java/com/example/demo/web/ProjectController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.domain.Project;
 import com.example.demo.service.MapValidationErrorService;
 import com.example.demo.service.ProjectService;
-
+@CrossOrigin
 @RestController
 @RequestMapping("api/project")
 public class ProjectController {
